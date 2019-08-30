ca991373b3824559f96e6b10b3af28d31e5714e6
OisinA <117397783@umail.ucc.ie>
2019-08-30 10:50:16 +0100

    Bring examples up to date

--- spec/compiler/std/print_spec.cr
+++ spec/compiler/std/print_spec.cr
@@ -14,11 +14,22 @@ describe Azula::Compiler do
             it "int" do
                 input = wrap_main("print(5);", "void")
                 compile_and_run(input).should eq "5"
+
+                input = wrap_main("int x = 10; print(x);", "void")
+                compile_and_run(input).should eq "10"
+            end
+
+            it "boolean" do
+                input = wrap_main("print(true);", "void")
+                compile_and_run(input).should eq "true"
+
+                input = wrap_main("bool x = false; print(x);", "void")
+                compile_and_run(input).should eq "false"
             end
 
             it "string concat" do
-                input = wrap_main("print(\"hi\", \"there\");", "void")
-                compile_and_run(input).should eq "hi there"
+                input = wrap_main("string y = \"friend\"; print(\"hi\", \"there\", y);", "void")
+                compile_and_run(input).should eq "hi there friend"
             end
 
         end
